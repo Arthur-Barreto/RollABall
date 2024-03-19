8db28f4e81a27bfd87d655b7b503731b9b557b7a
Arthur-Barreto <arthurbarreto2905@gmail.com>
2024-03-18 23:31:44 -0300

    timer + sound files

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,21 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 offset;
+    private Vector3 _offset;
     // Start is called before the first frame update
     void Start()
     {
-        offset = transform.position - player.transform.position;
+        _offset = transform.position - player.transform.position;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = player.transform.position + offset;
+        transform.position = player.transform.position + _offset;
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,52 +1,79 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 0;
+    public float speed;
     public TextMeshProUGUI countText;
     public GameObject winTextObject;
+    public GameObject gameOverTextObject;
+    // timer variables
+    public float timeRemaining = 149;
+    public bool timerIsRunning;
+    public TextMeshProUGUI timeText;
     
-    private Rigidbody rb;
-    private int count;
-    private bool isTen;
-    private float movementX;
-    private float movementY;
+    private Rigidbody _rb;
+    private int _count;
+    private bool _isTen;
+    private float _movementX;
+    private float _movementY;
+    private bool _isGameOver;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        count = 0;
-        isTen = false;
+        _rb = GetComponent<Rigidbody>();
+        _count = 0;
+        _isTen = false;
         
         SetCountText();
         winTextObject.SetActive(false);
+        gameOverTextObject.SetActive(false);
+
+        timerIsRunning = true;
     }
 
     void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
-        movementX = movementVector.x;
-        movementY = movementVector.y;
+        _movementX = movementVector.x;
+        _movementY = movementVector.y;
     }
 
     void SetCountText()
     {
-        countText.text = "Count: " + count.ToString();
-        if (count >= 10 && isTen)
+        countText.text = "Count: " + _count.ToString();
+        if (_count >= 10 && _isTen && !_isGameOver)
         {
             winTextObject.SetActive(true);
+            timerIsRunning = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime(timeRemaining);
+            }
+            else
+            {
+                gameOverTextObject.SetActive(true);
+                timeRemaining = 0;
+                timerIsRunning = false;
+                _isGameOver = true;
+            }
         }
     }
 
     void FixedUpdate()
     {
-        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-        rb.AddForce(movement * speed);
+        Vector3 movement = new Vector3(_movementX, 0.0f, _movementY);
+        _rb.AddForce(movement * speed);
     }
 
     void OnTriggerEnter(Collider other)
@@ -54,14 +81,24 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("PickUp"))
         {
             other.gameObject.SetActive(false);
-            count += 1;
+            _count += 1;
             SetCountText();
         }
 
-        if (other.gameObject.CompareTag("Portal") && count >=10)
+        if (other.gameObject.CompareTag("Portal") && _count >=10)
         {
-            isTen = true;
+            _isTen = true;
             SetCountText();
         }
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay += 1;
+
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timeText.text = "T- " + $"{minutes:00}:{seconds:00}";
     }
 }
